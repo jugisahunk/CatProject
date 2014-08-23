57b31f9a1c5bd8c393577fd7c24561209b12557f
jugisahunk <jugisahunk@gmail.com>
2014-08-23 16:08:13 -0700

    Initial Committ

---
+++ Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatProject
+{
+    public class Cat
+    {
+        private int _timesPetted;
+
+        public string Pur()
+        {
+            return "Purrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr";
+        }
+
+        public int Pet()
+        {
+            return ++_timesPetted;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat purra = new Cat();
+
+            Console.WriteLine(purra.Pur());
+
+            Console.ReadLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(purra.Pet());
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
